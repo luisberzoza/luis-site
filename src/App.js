2a40febaaee5c4b2f9f92eb74bc8054b1cf45780
luisberzoza <73074337+luisberzoza@users.noreply.github.com>
2022-05-25 00:24:41 -0500

    setting up global css file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import './App.css';
 import {Routes, Route} from 'react-router-dom';
+import Home from './containers/home';
+import About from './containers/about';
+import Contact from './containers/contact';
+import Portfolio from './containers/portfolio';
+import Skills from './containers/skills';
 
 function App() {
   return (
@@ -11,6 +16,11 @@ function App() {
       {/* main page content */}
       <Routes>
         {/* create routes */}
+        <Route path='/' index element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/contact' element={<Contact/>} />
+        <Route path='/portfolio' element={<Portfolio/>} />
+        <Route path='/skills' element={<Skills/>} />
       </Routes>
 
     </div>
