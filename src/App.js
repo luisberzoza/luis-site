565c1a2b05937e513ab1a89a41145dca18b1cd11
luisberzoza <73074337+luisberzoza@users.noreply.github.com>
2022-05-24 21:25:45 -0500

    adding components and router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import {Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <h1>NEW HOME PAGE / PORTFOLIO</h1>
+      {/* particle js */}
+
+      {/* navbar component */}
+
+      {/* main page content */}
+      <Routes>
+        {/* create routes */}
+      </Routes>
+
     </div>
   );
 }
